0e165f92e8a6705c3523dd29f185df983a4d7f2d
Chris Doble <chris@chrisdoble.com>
2023-10-28 10:36:58 +1100

    Update simulations to draw at the correct scale

--- simulations/draw_inverted_pendulum.m
+++ simulations/draw_inverted_pendulum.m
@@ -1,37 +1,39 @@
 (* ::Package:: *)
 
-drawInvertedPendulum[\[Theta]_, x_] := Module[
+drawInvertedPendulum[l_, \[Theta]_, x_] := Module[
   {
-    cartHeight = 1,
-    cartWidth = 2,
-    pendulumLength = 3,
-    pendulumRadius = 0.25
+    cartHeight = l / 2,
+    cartWidth = l,
+    pendulumRadius = l / 10
   },
-  Graphics[
-    {
-      (* Cart *)
-      EdgeForm[{Black, Thick}],
-      FaceForm[],
-      Rectangle[
-        {x - cartWidth / 2, 0},
-        {x + cartWidth / 2, cartHeight
-      }],
+  Magnify[
+    Graphics[
+      {
+        (* Cart *)
+        EdgeForm[{Black, Thick}],
+        FaceForm[],
+        Rectangle[
+          {x - cartWidth / 2, 0},
+          {x + cartWidth / 2, cartHeight
+        }],
 
-      (* Pendulum *)
-      Line[{
-        {x, cartHeight / 2},
-        {x - pendulumLength Sin[\[Theta]], cartHeight / 2 + pendulumLength Cos[\[Theta]]}
-      }],
-      FaceForm[Black],
-      Disk[
-        {
-          x - (pendulumLength + pendulumRadius) Sin[\[Theta]],
-          cartHeight / 2 + (pendulumLength + pendulumRadius) Cos[\[Theta]]
-        },
-        pendulumRadius
-      ]
-    },
-    Axes -> True,
-    PlotRange -> {{-10, 10}, {-5, 5}}
+        (* Pendulum *)
+        Line[{
+          {x, cartHeight / 2},
+          {x - l Sin[\[Theta]], cartHeight / 2 + l Cos[\[Theta]]}
+        }],
+        FaceForm[Black],
+        Disk[
+          {
+            x - (l + pendulumRadius) Sin[\[Theta]],
+            cartHeight / 2 + (l + pendulumRadius) Cos[\[Theta]]
+          },
+          pendulumRadius
+        ]
+      },
+      Axes -> True,
+      PlotRange -> {{-10l, 10l}, {-l, 2l}}
+    ],
+    2
   ]
 ];
