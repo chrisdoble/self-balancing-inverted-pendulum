290d0c8679b5084238884323d21de555b4c63087
Chris Doble <chris@chrisdoble.com>
2024-02-02 11:26:31 +1100

    Continue animating the equations of motion section

--- video/src/1_equations_of_motion.py
+++ video/src/1_equations_of_motion.py
@@ -4,6 +4,7 @@ from manim import (
     Circle,
     DashedLine,
     DEFAULT_FONT_SIZE,
+    DOWN,
     FadeIn,
     FadeOut,
     LEFT,
@@ -14,7 +15,11 @@ from manim import (
     Rectangle,
     there_and_back,
     PI,
+    rate_functions,
+    RED,
     RIGHT,
+    Transform,
+    TransformMatchingTex,
     UP,
     ValueTracker,
     VGroup,
@@ -198,8 +203,154 @@ class EquationsOfMotion(MovingCameraScene):
             r"\mathcal{L} = T - U", font_size=LARGE_FONT_SIZE
         ).move_to([12, -4, 0])
         self.play(FadeIn(general_lagrangian))
-        self.wait(2)
+        self.wait(2.5)
         self.play(FadeOut(general_lagrangian))
+        self.wait(3)
+
+        # Cart kinetic energy
+        cart_kinetic_energy_1 = MathTex(
+            r"{{T_\text{cart} = \frac{1}{2} m_1}} {{v}}^2", font_size=LARGE_FONT_SIZE
+        ).move_to([12, -4, 0])
+        self.play(FadeIn(cart_kinetic_energy_1))
+        self.wait(8)
+
+        cart_kinetic_energy_2 = (
+            MathTex(
+                r"{{T_\text{cart} = \frac{1}{2} m_1}} {{\dot{x}}}^2",
+                font_size=LARGE_FONT_SIZE,
+            )
+            .move_to(cart_kinetic_energy_1.get_center())
+            .set_color_by_tex("dot", RED)
+        )
+        self.play(TransformMatchingTex(cart_kinetic_energy_1, cart_kinetic_energy_2))
+        self.wait(3)
+        self.play(cart_kinetic_energy_2.animate.set_color_by_tex("dot", WHITE))
+        self.wait(6)
+        self.play(cart_kinetic_energy_2.animate.move_to([0, -4, 0]))
+        self.wait(6)
+
+        # Cart potential energy
+        cart_potential_energy = MathTex(
+            r"U_\text{cart} = 0", font_size=LARGE_FONT_SIZE
+        ).move_to([12, -4, 0])
+        self.play(FadeIn(cart_potential_energy))
+        self.wait(3.5)
+        self.play(
+            cart_potential_energy.animate.move_to([0, -6, 0]).align_to(
+                cart_kinetic_energy_2, LEFT
+            )
+        )
+        self.wait(2.5)
+
+        self.play(cart.animate(rate_func=there_and_back, run_time=2).shift([-1, 0, 0]))
+        self.wait(1)
+        self.play(
+            pendulum_angle.animate(rate_func=there_and_back, run_time=2).set_value(
+                PI / 4
+            )
+        )
         self.wait(2)
+
+        # Pendulum coordinates
+        pendulum_x_coordinate = MathTex(
+            r"X = x - l \sin \theta", font_size=LARGE_FONT_SIZE
+        )
+        pendulum_y_coordinate = (
+            MathTex(r"Y = l \cos \theta", font_size=LARGE_FONT_SIZE)
+            .next_to(pendulum_x_coordinate, DOWN, buff=0.75)
+            .align_to(pendulum_x_coordinate, LEFT)
+        )
+        pendulum_coordinates = VGroup(
+            pendulum_x_coordinate, pendulum_y_coordinate
+        ).move_to([12, -4, 0])
+        self.play(FadeIn(pendulum_coordinates))
+        self.wait(5)
+
+        # Pendulum velocities
+        pendulum_x_velocity = (
+            MathTex(
+                r"\dot{X} = \dot{x} - l \dot{\theta} \cos \theta",
+                font_size=LARGE_FONT_SIZE,
+            )
+            .align_to(pendulum_x_coordinate, DOWN)
+            .align_to(pendulum_x_coordinate, LEFT)
+        )
+        pendulum_y_velocity = (
+            MathTex(r"\dot{Y} = -l \dot{\theta} \sin \theta", font_size=LARGE_FONT_SIZE)
+            .align_to(pendulum_y_coordinate, DOWN)
+            .align_to(pendulum_y_coordinate, LEFT)
+        )
+        pendulum_velocities = VGroup(pendulum_x_velocity, pendulum_y_velocity)
+        self.play(FadeOut(pendulum_coordinates), FadeIn(pendulum_velocities))
+        self.wait(5.5)
+
+        # Pendulum velocity squared
+        pendulum_velocity_squared = MathTex(
+            r"V^2 = {{\dot{x}^2 - 2 l \dot{\theta} \dot{x} \cos \theta + l^2 \dot{\theta}^2}}",
+            font_size=LARGE_FONT_SIZE,
+        ).move_to([12, -4, 0])
+        self.play(FadeOut(pendulum_velocities), FadeIn(pendulum_velocity_squared))
+        self.wait(2.5)
+
+        # Pendulum kinetic energy
+        pendulum_kinetic_energy = MathTex(
+            r"T_\text{pendulum} = \frac{1}{2} m_2 ({{\dot{x}^2 - 2 l \dot{\theta} \dot{x} \cos \theta + l^2 \dot{\theta}^2}})",
+            font_size=LARGE_FONT_SIZE,
+        ).move_to([13.3, -4, 0])
+        self.play(
+            TransformMatchingTex(pendulum_velocity_squared, pendulum_kinetic_energy)
+        )
+        self.wait(2)
+        self.play(
+            pendulum_kinetic_energy.animate.move_to([0, -7.9, 0]).align_to(
+                cart_potential_energy, LEFT
+            )
+        )
+        self.wait(4)
+        self.play(
+            pendulum_angle.animate(rate_func=there_and_back, run_time=2).set_value(
+                PI / 4
+            )
+        )
+        self.wait(4)
+        self.play(
+            pendulum_angle.animate(
+                rate_func=rate_functions.ease_in_out_quad, run_time=2
+            ).set_value(PI / 2)
+        )
+        self.wait(2)
+
+        # Pendulum potential energy
+        pendulum_potential_energy = MathTex(
+            r"U_\text{pendulum} = m_2 g l \cos \theta", font_size=LARGE_FONT_SIZE
+        ).move_to([12, -4, 0])
+        self.play(
+            FadeIn(pendulum_potential_energy),
+            pendulum_angle.animate(
+                rate_func=rate_functions.ease_in_out_quad, run_time=2
+            ).set_value(PI / 9),
+        )
+        self.wait(2)
+        self.play(
+            pendulum_potential_energy.animate.move_to([0, -10, 0]).align_to(
+                pendulum_kinetic_energy, LEFT
+            )
+        )
+        self.wait(3)
+
+        # Lagrangian
+        lagrangian = MathTex(
+            r"\mathcal{L} = \frac{1}{2} (m_1 + m_2) \dot{x}^2 + \frac{1}{2} m_2 (l^2 \dot{\theta}^2 - 2 l \dot{\theta} \dot{x} \cos \theta) - m_2 g l \cos \theta",
+            font_size=LARGE_FONT_SIZE,
+        ).move_to([9, -6, 0])
+        self.play(
+            FadeOut(
+                cart_kinetic_energy_2,
+                cart_potential_energy,
+                pendulum_kinetic_energy,
+                pendulum_potential_energy,
+            ),
+            FadeIn(lagrangian),
+        )
 
         self.wait()
